6e0e20e2b5477ec48077489d56ca963ef7f9cecf
Sanket Hegde <sanket.hegde@optym.com>
2024-03-28 14:05:35 +0530

    first commit

---
+++ src/apiServices.js
@@ -0,0 +1,124 @@
+export class API {
+    // static getCatPetDonDetails() {
+    //     return fetch('http://127.0.0.1:8000/api/cpd/', {
+    //         method: 'GET',
+    //         headers: {
+    //             'Content-Type': 'application/json'
+    //         }
+    //     }).then(resp => resp.json())
+    //     // .then(resp => console.log(resp))
+    //     // .catch(err => console.log(err))
+    // }
+    
+    
+    static getPetDetails() {
+        return fetch('https://pet-adoption-application.onrender.com/api/pet_details/', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }).then(resp => resp.json())
+        // .then(resp => console.log(resp))
+        // .catch(err => console.log(err))
+    }
+
+    static postPetDetails(body) {
+        return fetch('https://pet-adoption-application.onrender.com/api/pet_details/', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(body)
+        }).then( resp => resp.json() )
+    }
+    static deletePetDetails(body) {
+        return fetch('https://pet-adoption-application.onrender.com/api/pet_details/', {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(body)
+        }).then( resp => resp.json() )
+    }
+    static getCategory() {
+        return fetch('https://pet-adoption-application.onrender.com/api/category/', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }).then(resp => resp.json())
+        // .then(resp => console.log(resp))
+        // .catch(err => console.log(err))
+    }
+
+    static postCategory(body) {
+        return fetch('https://pet-adoption-application.onrender.com/api/category/', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(body)
+        }).then( resp => resp.json() )
+        // .then(res => console.log(res))
+    }
+
+    static getLoginDetails() {
+        // return fetch('http://127.0.0.1:8000/api/login_details/', {
+        return fetch('https://pet-adoption-application.onrender.com/api/login_details/', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }).then(resp => resp.json())
+        // .then(resp => console.log(resp))
+        // .catch(err => console.log(err))
+    }
+
+    static postLoginDetails(body) {
+        return fetch('https://pet-adoption-application.onrender.com/api/login_details/', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(body)
+        }).then( resp => resp.json() )
+    }
+    static putLoginDetails(body) {
+        return fetch('https://pet-adoption-application.onrender.com/api/login_details/', {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(body)
+        }).then( resp => resp.json() )
+    }
+    static getAdoptions() {
+        return fetch('https://pet-adoption-application.onrender.com/api/adoption_details/', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }).then(resp => resp.json())
+        // .then(resp => console.log(resp))
+        // .catch(err => console.log(err))
+    }
+    static postAdoptionDetails(body) {
+        return fetch('https://pet-adoption-application.onrender.com/api/adoption_details/', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(body)
+        }).then( resp => resp.json() )
+    }
+    static getDonations() {
+        return fetch('https://pet-adoption-application.onrender.com/api/donations/', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }).then(resp => resp.json())
+        // .then(resp => console.log(resp))
+        // .catch(err => console.log(err))
+    }
+}
